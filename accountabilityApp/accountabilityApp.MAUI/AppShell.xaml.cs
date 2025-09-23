ff2838b5400839491881e789c8e37bdd32ace646
Bryson Price <7bjprice@gmail.com>
2025-09-23 14:07:33 -0600

    Add files via upload

---
+++ accountabilityApp/accountabilityApp.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+namespace accountabilityApp.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ accountabilityApp/accountabilityApp.MAUI/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+using accountabilityApp;
+
+namespace accountabilityApp.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MyApplication evergineApplication;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.evergineApplication = new MyApplication();
+            this.evergineView.Application = this.evergineApplication;
+        }
+    }
+}
