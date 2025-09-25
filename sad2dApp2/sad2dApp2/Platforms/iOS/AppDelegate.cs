2911af31c33d2d80834052df77d20aff44a6a179
kingminor <michaelgbrightman@yahoo.com>
2025-09-25 13:57:29 -0600

    Add initial .NET MAUI multi-platform app
    Created a new .NET MAUI application with platform-specific files for Android, iOS, Windows, MacCatalyst, and Tizen. Added main app structure, resources, styles, fonts, images, and configuration files to support cross-platform development.

---
+++ sad2dApp2/sad2dApp2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace sad2dApp2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
