94a0960ccafbbabbfdc9721290cffcbf1f9e7757
kingminor <michaelgbrightman@yahoo.com>
2025-09-30 14:05:59 -0600

    Update AcountaGotchi.cs

--- sad2dApp2/sad2dApp2/AcountaGotchi.cs
+++ sad2dApp2/sad2dApp2/AcountaGotchi.cs
@@ -9,29 +9,40 @@ namespace sad2dApp2
     class AcountaGotchi
     {
         public string Name { get; set; }
-        public int Happiness { get; set; }
-        public int Wellness { get; set; }
+        public float Happiness { get; set; }
+        public float Wellness { get; set; }
+        public DateTime StartDate { get; }
+        public DateTime LastLogin { get; set; }
 
-        int happinessDecayRate = 6;
-        int wellnessDecayRate = 6;
+        float hourlydecay = 0.25f;
 
-        public AcountaGotchi(string name)
+        public AcountaGotchi(string name) //This function is used when creating a tomagotchi for the first time
         {
             Name = name;
             Happiness = 75;
             Wellness = 75;
+            StartDate = DateTime.Now;
+            LastLogin = DateTime.Now;
         }
 
-        public AcountaGotchi(string name, int happiness, int wellness)
+        public AcountaGotchi(string name, float happiness, float wellness, DateTime startDate, DateTime lastLogin) //Used when loading it from json
         {
             Name = name;
             Happiness = happiness;
             Wellness = wellness;
+            StartDate = startDate;
+            LastLogin = lastLogin;
+            UpdateStatsAfterLoad();
         }
 
-        public void CompleteGoal()
+        public void UpdateStatsAfterLoad()
         {
+            int hoursSinceLastLogin = (int)(DateTime.Now - LastLogin).TotalHours;
+            float decayAmount = hoursSinceLastLogin * hourlydecay;
+            Happiness = Math.Max(0, Happiness - decayAmount);
+            Wellness = Math.Max(0, Wellness - decayAmount);
 
+            LastLogin = DateTime.Now;
         }
     }
 }
