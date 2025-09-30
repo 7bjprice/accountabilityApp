9f35322b488662dead343bdf8727ca8c6395a9ff
kingminor <michaelgbrightman@yahoo.com>
2025-09-30 13:46:17 -0600

    Add AcountaGotchi class and update MainPage UI
    Introduced the AcountaGotchi class to model a virtual pet with happiness and wellness properties. Updated MainPage.xaml to replace 'Sleep', 'Readyness', and 'Stabbyness' labels and progress bars with a single 'Wellness' indicator for improved clarity.

---
+++ sad2dApp2/sad2dApp2/AcountaGotchi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sad2dApp2
+{
+    class AcountaGotchi
+    {
+        public string Name { get; set; }
+        public int Happiness { get; set; }
+        public int Wellness { get; set; }
+
+        int happinessDecayRate = 6;
+        int wellnessDecayRate = 6;
+
+        public AcountaGotchi(string name)
+        {
+            Name = name;
+            Happiness = 75;
+            Wellness = 75;
+        }
+
+        public AcountaGotchi(string name, int happiness, int wellness)
+        {
+            Name = name;
+            Happiness = happiness;
+            Wellness = wellness;
+        }
+
+        public void CompleteGoal()
+        {
+
+        }
+    }
+}
