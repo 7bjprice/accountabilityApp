22d5821da90813cf028c1de34cd607652408daac
kingminor <michaelgbrightman@yahoo.com>
2025-10-02 13:52:23 -0600

    Add save/load system for AcountaGotchi
    Introduced SaveSystem for serializing and deserializing AcountaGotchi objects to local storage. Updated AcountaGotchi to be public and to save on creation. MainPage now loads or creates the first AcountaGotchi on startup.

--- sad2dApp2/sad2dApp2/MainPage.xaml.cs
+++ sad2dApp2/sad2dApp2/MainPage.xaml.cs
@@ -5,10 +5,26 @@ namespace sad2dApp2
 {
     public partial class MainPage : ContentPage
     {
+        // Make property type accessibility match property accessibility
+        public AcountaGotchi? CurrentAcountaGotchi { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+
+            // Load First AcountaGotchi
+            var acountaGotchiNames = SaveSystem.GetAllAcountaGotchiNames();
+            if (acountaGotchiNames.Count == 0)
+            {
+                // No AcountaGotchi found, create a new one
+                var newAcountaGotchi = new AcountaGotchi("Default");
+                acountaGotchiNames.Add(newAcountaGotchi.Name);
+                CurrentAcountaGotchi = newAcountaGotchi;
+            }
+            else //Load first acountaGotchi
+            {
+                CurrentAcountaGotchi = SaveSystem.LoadAcountagotchi(acountaGotchiNames[0]);
+            }
         }
 
         private async void OnBudgetClicked(object sender, EventArgs e)
